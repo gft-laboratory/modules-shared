e454a6d936ed275acf9d2eed029e12ad3c135f75
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2025-07-28 17:05:10 -0300

    fix module RDS-AuroraGlobal

--- Glue/glue-data-quality/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ruleset_name" {
-  description = "Nome do Data Quality Ruleset"
-  value       = aws_glue_data_quality_ruleset.this.name
-}
-
-output "ruleset_arn" {
-  description = "ARN do Data Quality Ruleset"
-  value       = aws_glue_data_quality_ruleset.this.arn
-}
